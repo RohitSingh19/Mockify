146c0b0622c80f7a5c3669a21580ff6a950dec6b
Rohit Singh <Rohit.Singh@clario.com>
2025-02-02 17:21:06 +0530

    All categories getAPI introduced

---
+++ backend/Mockify/Mockify.API/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using Bogus;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mockify.API.Models;
+using System.Reflection;
+
+namespace Mockify.API.Controllers
+{
+    [Route("api/v1")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet("getCategories")]
+        public IActionResult GetUsersMock()
+        {
+            var data = from t in Assembly.GetExecutingAssembly().GetTypes()
+                       where t.IsClass && t.Namespace == "Mockify.API.Models"
+                       select t;
+            
+            List<string> categories = data.Select(x=> x.Name).ToList();
+
+            return Ok(categories);
+        }
+    }
+}
