624f4a7b492279556225301b9d468130022f4bd1
Rohit Singh <Rohit.Singh@clario.com>
2025-02-05 19:28:21 +0530

    Added Material design

--- frontend/mockify/src/app/app.component.ts
+++ frontend/mockify/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSlideToggleModule,MatFormFieldModule, MatSelectModule, MatInputModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
