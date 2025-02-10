593790282fdafdd4f08d35b714926fadbc5d3fce
Rohit Singh <Rohit.Singh@clario.com>
2025-02-10 19:55:30 +0530

    Environment included

--- frontend/mockify/src/app/app.component.ts
+++ frontend/mockify/src/app/app.component.ts
@@ -1,19 +1,12 @@
 import { Component } from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
-import {MatSelectModule} from '@angular/material/select';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatGridListModule} from '@angular/material/grid-list';
 import {MatCard} from '@angular/material/card';
 import { CategoryComponent } from "./shared/category/category.component";
 import { PreviewComponent } from "./shared/preview/preview.component";
 
-
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [PreviewComponent, MatFormFieldModule, MatSelectModule, MatInputModule, FormsModule, MatGridListModule, MatCard, CategoryComponent, PreviewComponent],
+  imports: [PreviewComponent, MatCard, CategoryComponent, PreviewComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ frontend/mockify/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiUrl: 'http://mockify.runasp.net/api/v1/'
+};
