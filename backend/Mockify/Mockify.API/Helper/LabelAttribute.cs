4d1c6a9c46a1791ec759088bf4f64bf9d7e11f67
Rohit Singh <Rohit.Singh@clario.com>
2025-02-14 20:49:41 +0530

    CustomCategory API included

---
+++ backend/Mockify/Mockify.API/Helper/LabelAttribute.cs
@@ -0,0 +1,13 @@
+﻿namespace Mockify.API.Helper
+{
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = true)]
+    public class LabelAttribute : System.Attribute
+    {
+        public readonly string Text;
+
+        public LabelAttribute(string Text)
+        {
+            this.Text = Text;
+        }
+    }
+}
