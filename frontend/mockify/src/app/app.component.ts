eb4c04a088dac89e69a9a72b546533da906f7136
Rohit Singh <Rohit.Singh@clario.com>
2025-03-03 22:31:22 +0530

    Login UI improvised

--- frontend/mockify/src/app/app.component.ts
+++ frontend/mockify/src/app/app.component.ts
@@ -1,80 +1,46 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import {MatCard} from '@angular/material/card';
 import { CategoryComponent } from "./shared/category/category.component";
 import { PreviewComponent } from "./shared/preview/preview.component";
-import { GoogleLoginProvider, SocialLoginModule, SocialAuthServiceConfig, SocialAuthService, SocialUser } from '@abacritt/angularx-social-login';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../environments/environment';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { AuthService } from './core/services/auth.service';
+import { MatMenuModule } from '@angular/material/menu';
+
 
 declare const google: any;
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [PreviewComponent, MatCard, CategoryComponent, PreviewComponent, SocialLoginModule],
+  imports: [PreviewComponent, MatCard, CategoryComponent, PreviewComponent, MatIconModule, MatToolbarModule, MatMenuModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  providers: [SocialAuthService, {
-    provide: 'SocialAuthServiceConfig',    
-    useValue: {
-      autoLogin: false,
-      lang: 'en',
-      providers: [
-        {
-          id: GoogleLoginProvider.PROVIDER_ID,
-          provider: new GoogleLoginProvider('---') // Replace with your Client ID
-        }
-      ],
-      onError: (err) => console.error(err)
-    } as SocialAuthServiceConfig
-  }]
 })
-export class AppComponent implements AfterViewInit {
-  user: SocialUser | null = null;
+export class AppComponent implements AfterViewInit, OnInit {
+
   title = 'mockify';
-  constructor(private socialAuthService: SocialAuthService, private httpClient: HttpClient) {
-    this.socialAuthService.authState.subscribe((user) => {
-      console.log('User:', user);
-    });
+ 
+
+   
+  constructor(private httpClient: HttpClient, private authService: AuthService) {
+    
+  }
+
+  loginWithGoogle() {
+    this.authService.signInWithGoogle();
+  }
+  ngOnInit(): void {
   }
 
 
   ngAfterViewInit(): void {
-    google.accounts.id.initialize({
-      client_id: '----', // Replace with your Client ID
-      callback: this.handleCredentialResponse.bind(this),
-      auto_select: true, // Disable auto-sign-in for FedCM compatibility
-      use_fedcm_for_prompt: false // Opt-in to FedCM (experimental)
-    });
-
-    google.accounts.id.renderButton(
-      document.getElementById('google-signin-button'),
-      {
-        type: 'standard',
-        size: 'large',
-        text: 'signin_with',
-        width: 300
-      }
-    );
-
-    // Disable One Tap UI (optional, as it’s deprecated)
-    //google.accounts.id.disableAutoSelect();
   }
 
   handleCredentialResponse(response: any): void {
     console.log('Credential response:', response);
     this.sendTokenToBackend(response.credential);
-  }
-
-  signOut(): void {
-    this.socialAuthService.signOut();
-    this.user = null;
-    google.accounts.id.disableAutoSelect();
-  }
-
-  signInWithGoogle(): void {
-    this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID);
   }
 
   sendTokenToBackend(token: string): void {
--- frontend/mockify/src/app/core/services/mock-data.service.ts
+++ frontend/mockify/src/app/core/services/mock-data.service.ts
@@ -4,6 +4,8 @@ import { environment } from "../../../environments/environment";
 import { Category, CustomMockDataRequest } from "../models/category.model";
 import { HttpClient } from "@angular/common/http";
 
+
+
 @Injectable({  
     providedIn: 'root'
  })
@@ -11,7 +13,8 @@ import { HttpClient } from "@angular/common/http";
  export class MockDataService {
     
     defaultLimit = 100;
-    constructor(private httpClient: HttpClient) { }
+    constructor(private httpClient: HttpClient) {
+     }
 
     private mockJsonData = new BehaviorSubject<any>({});
     getmockJsonData$ = this.mockJsonData.asObservable();
@@ -35,4 +38,6 @@ import { HttpClient } from "@angular/common/http";
     generateMockDataForCustomJson(customMockDataRequest: CustomMockDataRequest): Observable<any> {
         return this.httpClient.post<CustomMockDataRequest>(`${environment.apiUrl}custom/${this.defaultLimit}`, customMockDataRequest);
     }
+
+   
  }
