b01ed32ea35010e70440f6fc602b8be5fc710a0c
Rohit Singh <Rohit.Singh@clario.com>
2025-04-05 18:41:25 +0530

    Template APIs included

---
+++ backend/Mockify/Mockify.API/DTO/UserToken.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Mockify.API.DTO
+{
+    public class UserToken
+    {
+        public UserToken(string email, string token)
+        {
+            this.Email = email;
+            this.Token = token;
+        }
+        public string Email { get; set; }
+        public string Token { get; set; }
+    }
+}
